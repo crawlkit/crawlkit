a6fef9268232cc6416c4642f7df9936fc5975f66
Joscha Feth <joscha@feth.com>
2015-11-27 01:10:52 +1100

    docs: add API docs

--- src/index.js
+++ src/index.js
@@ -27,6 +27,13 @@ const followRedirectsKey = Symbol();
 const browserCookiesKey = Symbol();
 const retriesKey = Symbol();
 
+/**
+* Transforms a {Map} to an {Object} hash.
+*
+* @private
+* @param {Map} map The map to transform
+* @return {Object} The transformed key/value hash object.
+*/
 function transformMapToObject(map) {
     const result = {};
     map.forEach((value, key) => {
@@ -35,6 +42,11 @@ function transformMapToObject(map) {
     return result;
 }
 
+/**
+* Gets a finder definition of a {@link CrawlKit} instance.
+* @private
+* @return {Finder} the finder instance set via {@link CrawlKit#setFinder}.
+*/
 function getFinder(crawlerInstance) {
     return crawlerInstance[finderKey].finder;
 }
@@ -58,38 +70,100 @@ function getRunners(crawlerInstance) {
     return crawlerInstance[runnerKey];
 }
 
+/**
+* The protocol a URL without a protocol is written to.
+*
+* @private
+* @type {String}
+*/
+const defaultAbsoluteTo = 'http://'
+
+/**
+* The CrawlKit base class. This is where the magic happens.
+*/
 class CrawlKit {
+
+    /**
+    * Create a CrawlKit instance
+    * @constructor
+    * @param {String} [url] The start URL. Sets the {@link CrawlKit#url}.
+    * @return {CrawlKit} a new CrawlKit instance
+    */
     constructor(url) {
-        this.url = url;
-        this.defaultAbsoluteTo = 'http://';
+        if (url) {
+            this.url = url;
+        }
         this[runnerKey] = new Map();
         this[finderKey] = {};
+        this[browserCookiesKey] = [];
     }
 
+    /**
+    * Getter/setter for the timeout in ms for runners and finder functions.
+    * The timeout starts fresh for each runner.
+    *
+    * Values under zero are set to zero.
+    *
+    * @type {!integer}
+    * @default 10000 (10 seconds)
+    */
     set timeout(num) {
         this[timeoutKey] = parseInt(num, 10);
     }
 
+    /**
+    * @ignore
+    */
     get timeout() {
         return Math.max(0, this[timeoutKey] || 10000);
     }
 
+    /**
+    * Getter/setter for the concurrency of the crawler.
+    * This controls the amount of PhantomJS instances that will be spawned
+    * and used to work on found websites. Adapt this to the power of your machine.
+    *
+    * Values under one are set to one.
+    *
+    * @type {!integer}
+    * @default 1 (No concurrency)
+    */
     set concurrency(num) {
         this[concurrencyKey] = parseInt(num, 10);
     }
 
+    /**
+    * @ignore
+    */
     get concurrency() {
         return Math.max(1, this[concurrencyKey] || 1);
     }
 
+    /**
+    * Getter/setter for the start URL of the crawler.
+    * This is the URL that will be used as an initial endpoint for the crawler.
+    * If the protocol is omitted (e.g. URL starts with //), the URL will be rewritten to http://
+    * @type {String}
+    */
     set url(str) {
         this[urlKey] = str;
     }
 
+    /**
+    * @ignore
+    */
     get url() {
         return this[urlKey];
     }
 
+    /**
+    * With this method a {@link Finder} instance can be set for the crawler.
+    * A finder is used for link discovery on a website. It is run directly after page load
+    * and is optional (e.g. if you want to only work on a single page).
+    *
+    * @param {!Finder} finder The finder instance to use for discovery.
+    * @param {...*} [runnableParams] These parameters are passed to the function returned by {@link Finder#getRunnable} at evaluation time.
+    */
     setFinder(finder /* parameters... */) {
         if (!finder || typeof finder.getRunnable !== 'function') {
             throw new Error('Not a valid finder instance');
@@ -99,14 +173,39 @@ class CrawlKit {
         this[finderKey].parameters = Array.prototype.slice.call(arguments, 1);
     }
 
+    /**
+    * Getter/setter for the number of retries when a PhantomJS instance crashes on a page.
+    * When a PhantomJS instance crashes whilst crawling a webpage, this instance is shutdown
+    * and replaced by a new one. By default the webpage that failed in such a way will be
+    * re-queued. This member controls how often that re-queueing happens.
+    *
+    * Values under zero are set to zero.
+    *
+    * @type {!integer}
+    * @default 3 (try 2 more times after the first failure)
+    */
     set retries(n) {
         this[retriesKey] = parseInt(n, 10);
     }
 
+    /**
+    * @ignore
+    */
     get retries() {
         return Math.max(0, this[retriesKey] || 3);
     }
 
+    /**
+    * Allows you to add a runner that is executed on each crawled page.
+    * The returned value of the runner is added to the overall result.
+    * Runners run sequentially on each webpage in the order they were added.
+    * If a runner is crashing PhantomJS more than {@link CrawlKit#retries} times, subsequent {@link Runner}s are not executed.
+    *
+    * @see For an example see `examples/simple.js`. For an example using parameters, see `examples/advanced.js`.
+    * @param {!String} key The runner identificator. This is also used in the result stream/object.
+    * @param {!Runner} runner The runner instance to use for discovery.
+    * @param {...*} [runnableParams] These parameters are passed to the function returned by {@link Runner#getRunnable} at evaluation time.
+    */
     addRunner(key, runner /* args ... */) {
         if (!key) {
             throw new Error('Not a valid runner key');
@@ -123,30 +222,66 @@ class CrawlKit {
         });
     }
 
+    /**
+    * Getter/setter for the map of parameters to pass to PhantomJS.
+    * You can use this for example to ignore SSL errors.
+    * For a list of parameters, please refer to the [PhantomJS documentation]{@link http://phantomjs.org/api/command-line.html}.
+    *
+    * @type {!Object.<String,String>}
+    */
     set phantomParameters(params) {
         this[phantomParamsKey] = params;
     }
 
+    /**
+    * @ignore
+    */
     get phantomParameters() {
         return this[phantomParamsKey] || {};
     }
 
+    /**
+    * Getter/setter for the map of settings to pass to an opened page.
+    * You can use this for example for Basic Authentication.
+    * For a list of options, please refer to the [PhantomJS documentation]{@link http://phantomjs.org/api/webpage/property/settings.html}.
+    *
+    * @type {!Object.<String,*>}
+    */
     set phantomPageSettings(settings) {
         this[phantomPageSettingsKey] = settings;
     }
 
+    /**
+    * @ignore
+    */
     get phantomPageSettings() {
         return this[phantomPageSettingsKey] || {};
     }
 
+    /**
+    * Getter/setter for whether to follow redirects or not.
+    * When following redirects, the original page is not processed.
+    *
+    * @type {!boolean}
+    * @default false
+    */
     set followRedirects(value) {
       this[followRedirectsKey] = !!value;
     }
 
+    /**
+    * @ignore
+    */
     get followRedirects() {
       return this[followRedirectsKey] || false;
     }
 
+    /**
+    * Getter/setter for the cookies to set within PhantomJS.
+    * Each entry is supposed to be an object [following the PhantomJS spec]{@link http://phantomjs.org/api/webpage/method/add-cookie.html}.
+    *
+    * @type {!Array.<Object>}
+    */
     set browserCookies(cookies) {
       if (!(cookies instanceof Array)) {
           throw new Error('Not properly munchable');
@@ -154,10 +289,19 @@ class CrawlKit {
       this[browserCookiesKey] = cookies;
     }
 
+    /**
+    * @ignore
+    */
     get browserCookies() {
-      return this[browserCookiesKey] || [];
+      return this[browserCookiesKey];
     }
 
+    /**
+    * This method starts the crawling/scraping process.
+    *
+    * @param {boolean} [shouldStream=false] Whether to stream the results or use a Promise
+    * @return {(Stream|Promise.<Object>)} By default a Promise object is returned that resolves to the result. If streaming is enabled it returns a JSON stream of the results.
+    */
     crawl(shouldStream) {
         const self = this;
         const crawlTimer = new NanoTimer();
@@ -481,7 +625,7 @@ class CrawlKit {
 
                 addUrl = (u) => {
                     let url = urijs(u);
-                    url = url.absoluteTo(self.defaultAbsoluteTo);
+                    url = url.absoluteTo(defaultAbsoluteTo);
                     url.normalize();
                     url = url.toString();
 
